5cce5841b24faac6d2eec7c8dd745568b5ec7eda
Guízar López Roberto Antonio <roanguizarlo@ittepic.com>
2026-02-11 10:11:30 -0700

    style: pues quite cosas a ver si asi me deja sacar a mi otra cuenta que esta como zombi porque NO hice el commit desde ahi

--- fundamentosPOO/src/Punto.java
+++ fundamentosPOO/src/Punto.java
@@ -1,12 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
  *
  * @author Nappe
  */
 public class Punto {
-    
+
+    double x;
+    double y;
+    String label;
+
+    public Punto(double X, double Y, String label) {
+
+        this.x = X;
+        this.y = Y;
+        this.label = label;
+
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+
+    public void setX(double nX) {
+        this.x = nX;
+    }
+
+    public void setY(double nY) {
+        this.y = nY;
+    }
+
+    public void setLabel(String nLabel) {
+        this.label = nLabel;
+    }
+
+    @Override
+    public String toString() {
+        return (this.label + " (" + this.x + ", " + this.y + ")");
+    }
 }
